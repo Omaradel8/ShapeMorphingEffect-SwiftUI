4f181a821b23b8cb532bf2d3da517ee029c70c8d
Omar Adel <omar.adel888@hotmail.com>
2024-08-04 03:06:20 +0300

    Initial Commit

---
+++ ShapeMorphingEffect/ShapeMorphingEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeMorphingEffectApp.swift
+//  ShapeMorphingEffect
+//
+//  Created by Omar Adel on 04/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShapeMorphingEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
